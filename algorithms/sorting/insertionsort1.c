6d86619e681a562fb364756ffce0fc42fbc8ab40
Stephen Huh <stephenhuh@gmail.com>
2015-02-07 11:06:09 -0600

    Insertion sort 1

---
+++ algorithms/sorting/insertionsort1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void insertionSort(int ar_size, int *ar){
+    int tmp = ar[ar_size-1], i, j;
+    printf("%d", tmp);
+    for (i = 1; i < ar_size; i++){
+        if ( ar[ar_size-1-i] > tmp){
+            ar[ar_size-i] = ar[ar_size-1-i];
+        }
+        else 
+            ar[ar_size-i] = tmp;
+        for (j = 0; j < ar_size; j++)
+            printf("%d ", ar[j]);
+        printf("\n");
+    }
+    
+
+}
+
+int main (void){
+
+    int _ar_size;
+    scanf("%d", &_ar_size);
+    int _ar[_ar_size], _ar_i;
+    for (_ar_i = 0; _ar_i < _ar_size; _ar_i++) {
+        scanf("%d", &_ar[_ar_i]);
+    }
+    
+    insertionSort(_ar_size, _ar);
+
+    return 0;
+}
